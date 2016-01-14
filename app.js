241a2be7749a17f11f032d4c5bf6cca450627109
Oleg Baranok <oleg.baranok@gmail.com>
2016-01-14 20:46:09 +0600

    1 bug is fixed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var main = function () {
 	"use strict";
 	$(".comment-input button").on("click",function(event) {
-		var $new_comment = $("<p>"),
-			comment_text = $(".comment-input input").val();
-		$new_comment.text(comment_text);
-		$(".comment").append($new_comment);
+		var $new_comment;
+		if ($(".comment-input input").val() !== ""){
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$(".comment").append($new_comment);
+		}
 	});
 };
 $(document).ready(main);
