df13f319137d8d4349611e413412a5124ae7fc9b
Oleg Baranok <oleg.baranok@gmail.com>
2016-01-14 21:03:08 +0600

    Добавлены реакция на клавишу Enter и плавное появление комментариев

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ var main = function () {
 		var $new_comment;
 		if ($(".comment-input input").val() !== ""){
 			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
 			$(".comment").append($new_comment);
+			$new_comment.fadeIn();
+			$(".comment-input input").val("");
+		}
+	});
+	$(".comment-input input").on("keypress",function(event) {
+		var $new_comment;
+		if (event.keyCode === 13) {
+			if ($(".comment-input input").val() !== ""){
+				$new_comment = $("<p>").text($(".comment-input input").val());
+				$new_comment.hide();
+				$(".comment").append($new_comment);
+				$new_comment.fadeIn();
+				$(".comment-input input").val("");
+			}
 		}
 	});
 };
